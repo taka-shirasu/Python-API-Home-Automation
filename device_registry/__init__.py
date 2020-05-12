37bb57e1145d3297ff06c6c985fdc81c60b16f86
Mitsutaka80 <mitsutakashirasu@gmail.com>
2020-05-12 16:06:11 +0100

    1

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -59,7 +59,7 @@ class DeviceList(Resource):
         parser.add_argument('name', required=True)
         parser.add_argument('device_type', required=True)
         parser.add_argument('controller_gateway', required=True)
-
+ 
         #Parse the arguments into an object
         args = parser.parse_args()
 
